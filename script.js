a3a49f2554e769a01660ab70576a3c6bd9926349
a-piotrowski <adam.piotrowski@pioart.pl>
2022-03-30 20:31:22 +0200

    js

---
+++ script.js
@@ -0,0 +1,27 @@
+const z=5; //deklaracja zmiennej
+ let x=7; //deklaracja stałej
+x=2;
+
+
+// z nie moge zmienić bo to stała 
+
+console.log(x+z);
+
+let currentSlide=0;
+
+//STRING zawsze w cudzysłowie!!!
+const string="nazwa";
+
+//obiekty
+
+const project={
+    name:"Projekt 1",
+    date:"30.03.2022"
+}
+
+console.log(project.name);
+console.log(project.date)
+
+console.log("Projekt o nazwie " + project.name + "zostałstworzony "+ project.date)
+
+
